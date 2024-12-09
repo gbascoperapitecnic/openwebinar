2fe20cae053438dff95fb159b8f9f57646a0fb71
gbascoperapitecnic <gbascope@rapitecnic.com>
2024-12-09 12:48:41 +0100

    useNavigate y rutas parametricas

--- react-pokemon-project/src/App.jsx
+++ react-pokemon-project/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom"
 import HomePage from './pages/HomePage'
 import PokemonsPage from './pages/PokemonsPage'
+import PokemonPage from './pages/PokemonPage'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage/>} />
       <Route path='/pokemons' element={<PokemonsPage/>} />
+      <Route path='/pokemons/:id' element={<PokemonPage/>} />
     </Routes>
   )
 }
---
+++ react-pokemon-project/src/pages/PokemonPage.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+
+import PokemonList from '../components/PokemonList'
+import PokemonDetails from '../components/PokemonDetails'
+import PokemonDetails2 from '../components/PokemonDetails2'
+import { useNavigate, useParams } from 'react-router-dom'
+import { MoveLeft, MoveRight } from 'lucide-react';
+
+export default function PokemonPage() {
+
+    const {id} = useParams()
+    const [pokemon, setPokemon] = useState(null)
+
+    useEffect(() => {
+        //cuando cambie el elemento id, hacer el fetch
+        fetchPokemon(id)
+    }, [id])
+
+    const fetchPokemon = async (id) => {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            const data = await response.json()
+
+            setPokemon(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const navigate = useNavigate()
+
+    const goTo = (id) => {
+        navigate(`/pokemons/${id}`)
+    }
+
+
+  return (
+    <section className=''>
+        {
+            pokemon ? (
+                <div className='rounded-xl flex flex-col justify-center items-center min-h-screen text-xl w-fit mx-auto'>
+                    <div className='border rounded-lg p-2'>
+                        <h1>{pokemon.name.toUpperCase()}</h1>
+                        <img src={pokemon.sprites.front_default} className='w-[200px]'></img>
+                        <h3>HP: {pokemon.stats[0].base_stat}</h3>
+                        <h3>Attack: {pokemon.stats[1].base_stat}</h3>
+                        <h3 className='mb-5'>Defense: {pokemon.stats[2].base_stat}</h3>
+
+
+                        <button className='rounded-md p-2 border' onClick={()=> goTo(Number(id) -1)}>{<MoveLeft/>}</button>
+                        <button className='rounded-md p-2 border' onClick={()=> goTo(Number(id) +1)}>{<MoveRight/>}</button>
+
+
+                    </div>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )
+        }
+    </section>
+  )
+}
