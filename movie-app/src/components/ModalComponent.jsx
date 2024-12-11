580b5292ed59ac3ddfbc6762071d0bbdfb088bf8
gbascoperapitecnic <gbascope@rapitecnic.com>
2024-12-11 13:32:10 +0100

    icono si no hay imagen

--- movie-app/src/components/ModalComponent.jsx
+++ movie-app/src/components/ModalComponent.jsx
@@ -1,5 +1,5 @@
 import Modal from '@mui/material/Modal';
-import { Star, X } from 'lucide-react';
+import { CircleHelp, Star, X } from 'lucide-react';
 import { useContext, useEffect, useState } from 'react';
 import { MoviesContext } from '../context/movies.context';
 import {cn} from '../lib/utils'
@@ -30,11 +30,12 @@ export default function ModalComponent({open, handleClose, movie}) {
       aria-describedby="modal-modal-description"
       className='flex justify-center items-center'
     >
-     <div className='shadow-2xl rounded-xl p-8 w-[60rem] relative text-white'>
-      <img className='absolute top-0 left-0 w-full h-full object-cover z-[-1] brightness-[20%] ' src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} />
+     <div className={cn('shadow-2xl rounded-xl p-8 w-[60rem] relative text-white', !movie.backdrop_path && 'bg-black')}>
+      <img className='absolute top-0 left-0 w-full h-full object-cover z-[-1] brightness-[20%] ' src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt='imagen_fondo' />
+
       <div className='info-movie flex items-center justify-between'>
         <div className='flex flex-col'>
-          <h1 className='text-5xl py-2'>{movie.title}</h1>
+          <h1 className='text-5xl mb-3 font-semibold'>{movie.title}</h1>
           <p>Título original: {movie.original_title}</p>
           <p>Idioma original: {movie.original_language}</p>
           <p>Fecha de estreno: {movie.release_date}</p>
@@ -49,9 +50,27 @@ export default function ModalComponent({open, handleClose, movie}) {
       </div>
 
       <div className='my-5'>
-        <div className='grid grid-cols-4 w-full gap-4'>
-          <img className='col-span-1 object-cover w-full h-full' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
-          <img className='col-span-3 object-cover w-full h-full' src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} />
+        <div className='grid grid-cols-4 w-full gap-4 place-items-center'>
+          <div className='col-span-1 w-full h-full shadow-2xl'>
+              {
+                !movie.poster_path ? (
+                  <CircleHelp className='border w-full'/>
+                ) : (
+                  <img className='object-cover w-full h-full' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt='poster'/>
+                )
+              }
+          </div>
+
+          <div className='col-span-3 w-full h-full shadow-2xl'>
+              {
+                !movie.backdrop_path ? (
+                  <CircleHelp className='border w-full'/>
+                ) : (
+                  <img className='object-cover w-full h-full' src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt='backdrop'/>
+                )
+              }
+          </div>
+
         </div>
       </div>
 
@@ -65,7 +84,7 @@ export default function ModalComponent({open, handleClose, movie}) {
             )
           }
         </div>
-        <p>{movie.overview}</p>
+        <p className='text-lg'>{movie.overview}</p>
       </div>
 
       <button className='absolute right-0 top-0 p-2' onClick={handleClose}>
